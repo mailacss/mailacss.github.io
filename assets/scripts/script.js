0071f95a1ff46292d597511ab4c2126b1f9f7c69
Maila Cardoso <cardmaila19@gmail.com>
2022-11-03 09:41:29 -0300

    Altera gitinore

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+function esconde(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
+}
+
+function mostra(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "block")
+        document.getElementById(el).style.display = 'none';
+    else
+        document.getElementById(el).style.display = 'block';
+}

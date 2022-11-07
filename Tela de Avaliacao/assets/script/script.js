70773b25ac55755c488b246d900cc9b8239a8858
Maila Cardoso <cardmaila19@gmail.com>
2022-11-07 10:42:35 -0300

    Fix JS script

--- Tela de Avaliacao/assets/script/script.js
+++ Tela de Avaliacao/assets/script/script.js
@@ -1,22 +1,30 @@
-function esconde(el) {
-    var display = document.getElementById(el).style.display;
-    if(display == "none")
-        document.getElementById(el).style.display = 'block';
-    else
-        document.getElementById(el).style.display = 'none';
-}
-function mostra(el) {
-    var display = document.getElementById(el).style.display;
-    if(display == "flex")
-        document.getElementById(el).style.display = 'none';
-    else
-        document.getElementById(el).style.display = 'flex';
-}
-
-
+var ex = '';
 
 function pegaNota(clicked){
-    var ex = document.getElementById("selected");
+    ex = document.getElementById("selected");
     ex.innerHTML = "You selected " + clicked + " out of 5";
+}
+
+function esconde(el) {
+    var display = document.getElementById(el).style.display;
+    if (ex == '') {
+        alert("Please, select a rating.")
+    } else {
+        if(display == "none")
+            document.getElementById(el).style.display = 'block';
+        else
+            document.getElementById(el).style.display = 'none';
+    }
 
 }
+
+function mostra(el) {
+    if (ex !== '') {
+        var display = document.getElementById(el).style.display;
+        if(display == "flex")
+            document.getElementById(el).style.display = 'none';
+        else
+            document.getElementById(el).style.display = 'flex';
+    }
+}
+

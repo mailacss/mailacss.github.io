834a2b5d50fcd23ecef64e71527843f73a444b54
Maila Cardoso <cardmaila19@gmail.com>
2022-11-10 17:17:42 -0300

    Start API

--- Pokedex/assets/script/script.js
+++ Pokedex/assets/script/script.js
@@ -1,11 +1,28 @@
-var pessoa = "Maila Cardoso"
-var nomm = "Pessoa"+pessoa
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=5'
 
-
-const teste = nomm = {
-    nome: nomm,
-    idade: 20
+function convert(pokemon){
+    return `    
+    <ol>
+        <li>
+            <a href="${pokemon.url}">${pokemon.name}</a>
+        </li>
+    </ol>
+`
 }
 
-console.log(nomm)
-console.log(nomm)
+const pokemonUl = document.getElementById('pokemonss');
+
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokList) => {
+        for (let index = 0; index < pokList.length; index++) {
+            const pokemon = pokList[index];
+            pokemonUl.innerHTML += convert(pokemon)
+        }
+    })
+
+    .catch((error) => console.error(error))
+
+    

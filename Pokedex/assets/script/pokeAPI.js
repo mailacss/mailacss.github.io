f440562bebd7e22d1c11e1c56663f4bc91ef781e
Maila Cardoso <cardmaila19@gmail.com>
2022-11-17 17:22:36 -0300

    dinamic done

--- Pokedex/assets/script/pokeAPI.js
+++ Pokedex/assets/script/pokeAPI.js
@@ -1,9 +1,19 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 12) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}` 
-    
+
     return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailReq) => Promise.all(detailReq))
+    .then((pokemonDetails) => pokemonDetails)
+
+
 }
--- Pokedex/assets/script/script.js
+++ Pokedex/assets/script/script.js
@@ -2,19 +2,20 @@ var offset = 0
 const limit = 12
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-
+function convertPokTypes(pokType) {
+    return pokType.map((typesSlot) => `<li >${typesSlot.type.name}</li>`)
+}
 
 function convert(pokemon){
 
     return `    
     <li class="pokemons">
             <h2>${pokemon.name}</h2>
-            <div id='categorias'>
-                <p>hab1</p>
-                <p>hab2</p>
-            </div>
-            <span>#num</span>
-            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png'></img>
+            <ol>
+                ${convertPokTypes(pokemon.types).join('')}
+            </ol>
+            <span>#${pokemon.order}</span>
+            <img src='${pokemon.sprites.other.home.front_default}'></img>
     </li>
 `
 }
@@ -25,17 +26,9 @@ pokemonUl.innerHTML = '';
 
 pokeApi.getPokemons().then((pokList = []) => {
         pokemonUl.innerHTML += pokList.map((convert)).join('')
+        document.getElementById("container").style.display = 'grid'
+
+
 
     })
 
-
-
-
-
-
-
-
-
-    function pula() {
-        return 0
-    }
